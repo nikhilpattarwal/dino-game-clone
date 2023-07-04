4c8da4deae00e0b1cc5a0b485a9eacbb6b26b71b
Nikhil <nikhilpatterwal123@gmail.com>
2023-07-04 13:33:42 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,95 @@
+import {setUpground, updateGround} from "./ground.js";
+import {getDinoRect, setDinoLoose, setUpDino, updateDino} from "./dino.js";  
+import {getCactusRects, setUpCactus, updateCactus} from "./cactus.js";  
+
+
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE =0.00001;
+const worldElem = document.querySelector('[data-world]');
+const scoreElem =document.querySelector('[data-score]');
+const dataStartScreen =document.querySelector('[data-start-screen]');
+
+setPixelToWorldScale()
+window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart, {once:true});
+
+
+
+let lastTime;
+let speedScale;
+let score;
+function update (time){
+  if(lastTime == null){
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;
+  }  
+  const delta = time - lastTime;
+ 
+  console.log(delta);
+  updateGround(delta, speedScale);
+  updateDino(delta, speedScale);
+  updateCactus(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
+
+  if(checkLoose()) return handleLoose();
+
+  lastTime = time;  
+  window.requestAnimationFrame(update);
+}
+
+function checkLoose(){
+    const dinoRect = getDinoRect();
+    return getCactusRects().some(rect => isCollision(rect, dinoRect));
+}
+
+function isCollision(rect1, rect2){
+    return (rect1.left < rect2.right && 
+    rect1.top <  rect2.bottom &&
+    rect1.right > rect2.left &&
+    rect1.bottom > rect2.top
+    )
+}
+
+function updateSpeedScale(delta){
+    speedScale += delta * SPEED_SCALE_INCREASE;
+}
+
+function updateScore(delta){
+    score+= delta * 0.01;
+    scoreElem.textContent = Math.floor(score); 
+}
+
+function handleStart(){
+    lastTime = null;
+    speedScale = 1;
+    score = 0;
+    dataStartScreen.classList.add("hide");
+    setUpground();
+    setUpDino();
+    setUpCactus();
+    window.requestAnimationFrame(update);
+}
+
+function handleLoose(){
+    setDinoLoose()
+    setTimeout(() =>{
+    document.addEventListener("keydown", handleStart, {once:true});
+    dataStartScreen.classList.add("hide");
+    },100)
+}
+
+function setPixelToWorldScale() {
+    let worldToPixelScale
+    if(window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT){
+        worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+    }else{
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
+    }
+
+    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+}
+
